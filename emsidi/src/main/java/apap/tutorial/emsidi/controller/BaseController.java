20f7005fa36c86e4db08dbd57ddf7a67bec07433
Suwandi Kurniawan <suwandikurniawan89@gmail.com>
2021-10-06 11:33:33 +0700

    feat: add footer with time now

--- emsidi/src/main/java/apap/tutorial/emsidi/controller/BaseController.java
+++ emsidi/src/main/java/apap/tutorial/emsidi/controller/BaseController.java
@@ -1,11 +1,19 @@
 package apap.tutorial.emsidi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalTime;
 
 @Controller
 public class BaseController {
 
     @GetMapping("/")
-    private String home() {return "home"; }
+    private String home(Model model) {
+        LocalTime waktuSekarang = LocalTime.now();
+        model.addAttribute("time",waktuSekarang);
+        return "home";
+    }
+
 }
